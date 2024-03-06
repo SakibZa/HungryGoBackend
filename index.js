91eea2d4ed7e82ab18bba443a1e6beac17b48a9c
sakibhusainzaidi2000@gmail.com <sakibhusainzaidi2000@gmail.com>
2024-03-06 21:37:36 +0530

    changes has been done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 require('dotenv').config();
 const port = process.env.PORT;
-const db = require('./config/mongoose');
+const db = require('./config/mongoose.js');
 const app = express();
 
 app.use((req, res, next) =>{
